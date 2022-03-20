3b0e598a0adc5aa14d51ccdb344e4ecd60e5d089
unknown <mk3739@gmail.com>
2022-03-20 15:35:09 -0700

    broken, but added tensorflow mobilenet

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+async function predict(img) {
+  const mobilenet = require('@tensorflow-models/mobilenet');
+  console.log('Loading mobilenet...');
+  console.log(img);
+  //Load the model
+  const model = await mobilenet.load();
+  console.log('Successfully loaded model')
+  // Make a prediction through the model on our image.
+  const prediction = await model.classify(img);
+  console.log('Predictions: ');
+  console.log(prediction);  
+}
+
+router.post('/', function (req, res) {
+  //check that req.body is an image
+  predict(req.body);
+  res.render(pre)
+}
+
 module.exports = router;
